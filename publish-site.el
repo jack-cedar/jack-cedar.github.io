fd2738f8b159e2415bf48b9ad06a3f18b1545976
Jack Cedar <jjanousekw@gmail.com>
2023-05-06 00:09:51 -0700

    updated style

--- publish-site.el
+++ publish-site.el
@@ -3,33 +3,35 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Initialize the package system
+;; Initialize package system
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; Install use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
 ;; Install dependencies
-(package-install 'htmlize)
+(use-package htmlize)
+(use-package esxml)
 
 
 ;; Load Publishing Utilities
 (require 'ox-publish)
 
 
+(setq make-backup-files nil)
+
 (setq org-html-head
       "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
       <style>
-        body {background-color: #232840}
-        li {color: #f9f871}
-        .title {color: #f9f871}
-        h2 {color: #d085d2}
-        p {color: #fafafa}
-        a {color: #31c8d5}
-        #nav_bar {padding: 10px;}
-        #home {text-align: center}
-      </style>"
-
-      )
+      body {color: #fafafa; background-color: #232830;}
+      .title {color: #f9f871}
+      h2 {color: #d085d2}
+      a {color: #31c8d5}
+      </style>")
 
 
 
