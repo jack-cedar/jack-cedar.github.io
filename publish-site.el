139d3a0a05c5dee46460886157bf142fad5338f9
Jack Cedar <jjanousekw@gmail.com>
2023-05-09 14:02:58 -0700

    stylesheet.org

--- publish-site.el
+++ publish-site.el
@@ -21,30 +21,24 @@
 ;; Load Publishing Utilities
 (require 'ox-publish)
 
+(org-babel-tangle-file "./stylesheets.org")
+
 
 (setq make-backup-files nil)
 
-(setq org-html-head
-      "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
-      <style>
-      body {color: #fafafa; background-color: #232840;}
-        .title {color: #f9f871}
-        h2 {color: #d085d2}
-        a {color: #31c8d5}
-      </style>"
-
-      )
+(setq 
+      
+    )
 
 
 
 (setq org-html-validation-link nil
       org-html-head-include-scripts nil
       org-html-head-include-default-style nil
+      org-html-head "<link rel=\"stylesheet\" href=\"./css/basic.css\" />"
       )
-      
-;; yellow #f9f871
-;; Purple #d085d2
-;; Light blu #31c8d5
+;;      <link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
+
 ;; Define Publishing Project
 (setq org-publish-project-alist
       (list
